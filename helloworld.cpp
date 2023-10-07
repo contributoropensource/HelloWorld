eb55934caa84a9b28730e09e20f1145668e630dc
mayanklakkewar <97389735+mayanklakkewar@users.noreply.github.com>
2023-10-07 09:27:30 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class hello{
+    public:
+    void display(){
+        cout << "hello world" << endl;
+    }
+};
+
+int main(){
+    cout << "HELLO WORLD" <<endl;
+    hello hi;
+    hi.display();
+}
+
